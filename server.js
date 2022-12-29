3d0e0e6cd808bad0f9b2307612f1c28cd72a9d34
shadrach i <itodoshadrach@gmail.com>
2022-12-29 15:19:03 +0100

    hgjgj

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use("/api/password", forgotpasswordRouter, resetPasswordRouter);
 //cors middleware
 app.use(
   cors({
-    origin: "https://shoppyme-shadrach.onrender.com/",
+    origin: "https://shoppyme-shadrach.onrender.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "GET"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
